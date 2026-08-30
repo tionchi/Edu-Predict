849fceab641eb86a03521741836680b2a3dccfff
tionchi <157604914+tionchi@users.noreply.github.com>
2026-08-30 05:58:05 +0100

    Refactor training script to correct variable naming and streamline model training process

--- src/train.py
+++ src/train.py
@@ -1,20 +1,22 @@
-import pandas as pd
+import pandas as pd#
+import joblib
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
+from sklearn.pipeline import Pipeline
 
 from preprocessing import create_preprocessor
 
 #Load dataset
 df = pd.read_csv("data/StudentPerformanceFactors.csv")
 
-x = df.drop("Exam_Score", axis=1)
+X = df.drop("Exam_Score", axis=1)
 y = df["Exam_Score"]
 
 #splitting data
-x_train, x_test, y_train, y_test = train_test_split(
-    x,
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
     y,
     test_size=0.2,
     random_state=42
@@ -22,20 +24,26 @@ x_train, x_test, y_train, y_test = train_test_split(
 #creating preprocessing system
 preprocessor = create_preprocessor()
 
-#process traaaining data
-x_train_processed = preprocessor.fit_transform(x_train)
-x_test_processed = preprocessor.transform(x_test)
+#Create ML pipeline
+model = Pipeline([
+    ("preprocessor", preprocessor),
+    ("regressor", LinearRegression())
+])
 
-#Create model
-model = LinearRegression()
-#Train Model
-model.fit(x_train_processed, y_train)
+#Train model
+model.fit(X_train, y_train)
 
 #Predict
-predictions=model.predict(x_test_processed)
+predictions = model.predict(X_test)
+
+#model eval
 mae = mean_absolute_error(y_test, predictions)
 
 #Output
 print("Mean Absolute Error", mae)
+
+#saving trained model
+joblib.dump(model, "models/student_performance_model.pkl")
+print("Model Saved Successfully.")
 
 
